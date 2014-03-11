955e11810556899eb0cdff8424f2f5778a0e8473
zimski <chadda.chakib@gmail.com>
2014-03-11 11:10:05 +0000

    prepare cms template

--- scripts/update_dhcp-vm/shell.sh
+++ scripts/update_dhcp-vm/shell.sh
@@ -1,3 +1,1 @@
-$$ send_file.send file_conf_dhcp_vm
-cat /etc/dhcp/dhcp_vm.conf
-/etc/init.d/isc-dhcp-server restart
+$$ send_file.send file_conf_dhcp_vmcat /etc/dhcp/dhcp_vm.conf/etc/init.d/isc-dhcp-server restart
