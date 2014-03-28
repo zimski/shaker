a54bc08986082cb6c63fc6f0a1559d507b58efb4
zimski <chadda.chakib@gmail.com>
2014-03-28 14:19:09 +0000

    fix template prompt

--- views/CRON_JOB/install.sh
+++ views/CRON_JOB/install.sh
@@ -4,6 +4,6 @@ redis-cli HSET M:CRON_JOB:Forms 2 "no:yes:Script name:script"
 redis-cli HSET M:CRON_JOB:Forms 3 "no:yes:Params:argv"
 redis-cli HSET M:CRON_JOB:Forms 4 "no:yes:Status:status"
 redis-cli HSET M:CRON_JOB:Forms 5 "no:yes:Description:desc"
-redis-cli HSET M:CRON_JOB:Button 0 "Red:Start:CRON_JOB_start:#id?user,password"
+redis-cli HSET M:CRON_JOB:Button 0 "Red:Start:CRON_JOB_start:#id?user,password,interface"
 redis-cli HSET M:CRON_JOB:Button 1 "Green:Stop:CRON_JOB_stop:"
 redis-cli RPUSH Shaker:module:list CRON_JOB
