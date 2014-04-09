805d84ad922ea47701847852e9491723828c735a
zimski <chadda.chakib@gmail.com>
2014-04-09 14:40:46 +0000

    Add halt to netDevice push cmds

--- py-sc/modules/netDevice.py
+++ py-sc/modules/netDevice.py
@@ -1,5 +1,6 @@
 import pxssh
-
+import nsGlobal
+import sys
 def push(ssh,data,sk):
     netssh = pxssh.pxssh()
     user = data['user']
@@ -8,6 +9,11 @@ def push(ssh,data,sk):
     netssh.login(hostname,user,password,auto_prompt_reset=False)
     list_cmd = data['conf'].split('\n')
     for cmd in list_cmd:
-        netssh.sendline(cmd)
-        sk(2,cmd+"\n")
+      # Check if we must halt ?!!!
+      if nsGlobal.halt:
+          sk(3,"["+str(nsGlobal.self_id)+"]  **** Halt ****")
+          ssh.logout()
+          sys.exit(0)
+      netssh.sendline(cmd)
+      sk(2,cmd+"\n")
     sk(3,'Push cmds finish ! :)')
